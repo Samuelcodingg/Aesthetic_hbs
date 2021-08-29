c6293c27eb966cda547e7c2f770ef99c7797d329
Samuelcodingg <samuelroman39@gmail.com>
2021-08-28 21:43:38 -0500

    layouting completed

---
+++ public/assets/js/main.js
@@ -0,0 +1,11 @@
+document.addEventListener('DOMContentLoaded', () => {
+
+    //animations
+    ScrollReveal().reveal('.slogan' , {
+        duration: 3000
+    })
+
+    ScrollReveal().reveal('.container-transparent', {
+        duration: 3000
+    })
+})
